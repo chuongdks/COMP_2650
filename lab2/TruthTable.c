b5f7520f4962de8f3a3c7750b3ebe9cfeb257b4c
Chuong Pham <phchuong1998@gmail.com>
2024-02-06 01:51:29 -0500

    Finalize version

--- lab2/TruthTable.c
+++ lab2/TruthTable.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 // Function to evaluate a single term
-int evalTerm(int A, int B, int C, char *term) {
+int evalTerm(int A, int B, int C, char *term) 
+{
     int result = 1; // Initialize result to 1 (true)
     int len = strlen(term);
     for (int i = 0; i < len; i++) 
@@ -35,45 +36,53 @@ int evalTerm(int A, int B, int C, char *term) {
 }
 
 int main(int argc, char *argv[]) {
-    // int A = 0, B = 0, C = 0;
-    int outputTemp = 0, outputFinal = 0;
+    //Check for correct argument number
     if (argc != 2) 
     {
-        printf("Usage: %s \"Boolean function\"\n", argv[0]);
+        printf("The format is: ./*name* \"ABC + AC'\"");
         return 1;
     }
-
     // Get the Boolean function from command-line argument
     char *function = argv[1];
-    // Get the first token, Tokenize the function based on '+'
-    char *term = strtok(function, " .,-:+");
     
-    // Initialize output outside the term loop
-    int output = 0;
-
-    //
-    while (term != NULL) 
+    printf("Truth table of %s:\n", function);
+    printf("A B C |  F\n");
+    printf("------|--\n");
+    // Print each term
+    for (int A = 0; A <= 1; A++) 
     {
-        // Print each term
-        printf("Term:%s\n", term);
-        for (int A = 0; A <= 1; A++) 
+        for (int B = 0; B <= 1; B++) 
         {
-            for (int B = 0; B <= 1; B++) 
+            for (int C = 0; C <= 1; C++) 
             {
-                for (int C = 0; C <= 1; C++) 
+                // Since strtok() function modified the string, use a temporary function and initilized a new string each loop
+                char *tempFunction = (char*) malloc (sizeof (function)*sizeof(char)) ;
+                strcpy (tempFunction,function);
+                // Get the first token of the tempFunction string, Tokenize the function based on '+'
+                char *term = strtok (tempFunction, " .,-:+");
+                // Initialize 'output' outside the tokenized while loop so that 'output' will reset and check for a new term (Ex: 000 -> 001 'output' will reset to 0)
+                int output = 0;
+
+                // Walk through other tokens 
+                while (term  != NULL)
                 {
                     // Evaluate the function for each combination of A, B, C
-                    int output = 0;
-                    output = evalTerm (A, B, C, term); //output || 
-                    //output1 = evalTerm (A, B, C, term1);
-                    //output2 = evalTerm (A, B, C, term2);
-                    //output =  output1 || output2;
-                    //printf("%d\t%d\t%d\t%d\n", A, B, C, output);
+                    output = output || evalTerm (A, B, C, term); 
+                    //printf("%s\n", term);
+                    term = strtok (NULL, " .,-:+");
                 }
+                printf("%d %d %d | %d\n", A, B, C, output);
+                //Free the tempFunction string
+                free(tempFunction);
             }
         }
-        // Move to the next term
-        term = strtok (NULL, "+");
     }
     return 0;
 }
+
+//Assign argument to each outputs, last resort if cant tokenize strings
+//output1 = evalTerm (A, B, C, term1);
+//output2 = evalTerm (A, B, C, term2);
+//output3 = evalTerm (A, B, C, term3);
+//output =  output1 || output2 || output3;
+//printf("%d\t%d\t%d\t%d\n", A, B, C, output);
