af4754600a8ba5ffa21e241108b365d53aac5ac4
Chuong Pham <phchuong1998@gmail.com>
2024-02-13 12:12:24 -0500

    Added better version of Turth Table

--- lab2/TruthTableArg.c
+++ lab2/TruthTableArg.c
@@ -47,60 +47,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //Change size depend on how many arguments, program will have error if does not scale the number of arguments
-    int size = 0;
-    if (argc <= 6)
-    {
-        size = argc/2;
-    }
-    else
-    {
-        size = argc*1000;
-    }
-    // Allocate memory for arrays
-    char *term[size]; // +1 for NULL Terminator if ever use argv/2 for the array number, but it could cause limited number of arguments
-    char op[size]; //Increase to argc*10 so that the program could take more than 9 arguments, 1 string argument is superior https://github.com/chuongdks/COMP_2650.git
-    
-    // Iterate over arguments
-    int i = 1;
-    int termCount = 0; // Use a counter instead of argv cuz argv might have more and it is more complicated
-    int opCount = 0; // If use argv to count the term and operator, will need more duck tape solution
-    while (argv[i] != NULL) 
-    {
-        if (i % 2 == 0) // If the argument position is even, it is an operator
-        {
-            op[opCount++] = argv[i][0]; // Increase the Operator Counter
-        }
-        else // Odd position, it is a term
-        {
-            term[termCount++] = argv[i]; // Increase the Term Counter
-        }
-        i++;
-    }
-
-    // Null-terminate the arrays to deal with weird symbols when print them out. Due to uninitialized memory or accessing beyond the allocated memory
-    op[opCount] = '\0'; // Null-terminate op array
-    term[termCount] = NULL;   // Null-terminate term array
-
     // Print terms
-    int j = 0;
     printf ("Truth Table of: ");
-    while (term[j] != NULL || op[j] != '\0') 
+    for (int i = 1; i < argc; i++)
     {
-        printf("%s %c ", term[j], op[j]);
-        j++;
-    }
-
-    // Check if operators is '+'
-    j = 0;
-    while (op[j] != '\0') 
-    {
-        if (op[j] != '+') 
-        {
-            printf("Error: Invalid operator. The correct arguments is: %s \"ABC\" + \"AC'\" + \"B'\" + \"BC'\"...\n", argv[0]);
-            return 1;
-        }
-        j++;
+        printf("%s ", argv[i]);
     }
 
     printf("\n");
@@ -117,14 +68,14 @@ int main(int argc, char *argv[]) {
                 int output = 0;
 
                 // Walk through other tokens 
-                for (int i = 0; i < termCount; i++)
+                for (int i = 1; i < argc; i++)
                 {
-                    // Evaluate the function for each combination of A, B, C
-                    output = output || evalTerm (A, B, C, term[i]); 
-                    //printf("%s\n", term);
+                    if (i % 2 != 0)
+                    {
+                        output = output || evalTerm (A, B, C, argv[i]); 
+                    }
                 }
                 printf("%d %d %d |  %d\n", A, B, C, output);
-                
             }
         }
     }
